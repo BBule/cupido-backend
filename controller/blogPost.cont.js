63579db89053ed92547c4567f39e571e399e8ba1
saikat <saikatchakrabortty2@gmail.com>
2019-04-17 21:43:11 +0530

    [ADD] BlogPost with model & necessery user APIS

---
+++ controller/blogPost.cont.js
@@ -0,0 +1,31 @@
+const BlogPost = require("../models/BlogPost");
+
+const getBlogPosts = async (page = 1, limit = 10, sortByPopularity = false) => {
+    let sortQuery = { createdAt: -1 };
+    if (sortByPopularity) {
+        sortQuery["popularity_hits"] = -1;
+    }
+    const allPosts = await BlogPost.find()
+        .select({
+            topic: 1,
+            title: 1,
+            time_to_read: 1,
+            author: 1,
+            createdAt: 1
+        })
+        .sort(sortQuery)
+        .skip(limit * (page - 1))
+        .limit(limit * (page - 1) + limit)
+        .exec();
+    const totalPostCount = await BlogPost.countDocuments().exec();
+    return {
+        currentPage: page,
+        maxPage: Math.ceil(totalPostCount / limit),
+        data: allPosts
+    };
+};
+const getBlogPostById = async id => {
+    return await BlogPost.findOne({ _id: id }).exec();
+};
+
+module.exports = { getBlogPosts, getBlogPostById };
---
+++ routes/blogPost.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const authenticate = require(".././middleware/authenticate");
+
+const BlogPostCont = require("../controller/blogPost.cont");
+
+router.get("/api/blogposts", (req, res, next) => {
+    const { page = 1, limit = 10, popularity = false } = req.query;
+    return BlogPostCont.getBlogPosts(page, limit, popularity)
+        .then(data => {
+            return res.json(data);
+        })
+        .catch(error => {
+            return next({
+                message:
+                    error.message || "unable to fetch blogs,please try again!",
+                status: 400
+            });
+        });
+});
+
+router.get("/api/blogposts/:id", (req, res, next) => {
+    return BlogPostCont.getBlogPostById(req.params.id)
+        .then(data => {
+            return res.json(data);
+        })
+        .catch(error => {
+            return next({
+                message:
+                    error.message || "unable to fetch blogs,please try again!",
+                status: 400
+            });
+        });
+});
+
+module.exports = router;
