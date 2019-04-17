a603d132f86f23c50d214275a4e829f67a2501ad
saikat <saikatchakrabortty2@gmail.com>
2019-04-17 18:27:47 +0530

    [UPDATE] timely event for 2 promotional event

--- agenda/index.js
+++ agenda/index.js
@@ -1,6 +1,16 @@
 var Agenda = require("agenda");
+const moment = require("moment");
 //import all necessery dependencies , middlewaresscripts .
+//Models
+const Commits = require("../models/mycommits");
+const Sales = require("../models/saleslist");
+const User = require("../models/user");
+
+//helpers
+const { SendMail, getEJSTemplate } = require("../helpers/mailHelper");
+
 const config = require("../config/config");
+
 const agenda = new Agenda({
     db: {
         address: config.MONGO_URL
@@ -8,15 +18,142 @@ const agenda = new Agenda({
     collection: "agendaJobs"
 });
 
-var start = async function() {
+var start = async () => {
     await agenda._ready;
 
     await agenda.start();
 
-    agenda.define("CheckForTxn and Send", (job, done) => {
-        // console.log(job.data);
-        const jobData = job.attrs.data;
-        const failCount = job.attrs.failCount || 0;
+    agenda.define("on start sale all user", (job, done) => {
+        /**
+         *  [PROMOTIONAL]
+         * Notification level for this should be made 1 from 0
+         * start time should be less and end time should be more ,
+         * in that above case it comes under when its started.
+         * notification_level represents how many times it sent notiifcation.
+         */
+        const salesQuery = {
+            notification_level: { $eq: 0 },
+            starttime: {
+                $lte: moment()
+                    .utcOffset(330)
+                    .toDate()
+            },
+            endtime: {
+                $gte: moment()
+                    .utcOffset(330)
+                    .toDate()
+            }
+        };
+
+        return Sales.findOne(salesQuery)
+            .exec()
+            .then(salesDoc => {
+                /**
+                 * Use cursor to loop over all users
+                 */
+                return User.find({
+                    notif_subscribe: true
+                })
+                    .select({ username: 1, email: 1 })
+                    .cursor()
+                    .on("data", async user => {
+                        const ejsTemplate = await getEJSTemplate({
+                            fileName: "sale_is_live.ejs"
+                        });
+                        const finalHTML = ejsTemplate({
+                            time: moment().format("lll"),
+                            username: user.name,
+                            saleDetails: salesDoc //may be format properly before passing it
+                        });
+                        const message = {
+                            to: user.email,
+                            subject:
+                                "Sale is now live! check out whats there for you.",
+                            body: finalHTML
+                        };
+                        return await SendMail(message);
+                    })
+                    .on("error", err => {
+                        done(err);
+                    })
+                    .on("end", () => {
+                        salesDoc.notification_level = 1;
+                        salesDoc.save();
+                        return true;
+                    });
+            })
+            .catch(error => {
+                done(error);
+            });
+    });
+    agenda.define("before end sale all user", (job, done) => {
+        /**
+         *  [PROMOTIONAL]
+         * Notification level for this should be made 1 from 2
+         * start time should be less and end time should be more ,
+         * in that above case it comes under when its about to end.
+         * notification_level represents how many times it sent notiifcation.
+         */
+        const salesQuery = {
+            notification_level: { $eq: 1 },
+            starttime: {
+                $lte: moment()
+                    .utcOffset(330)
+                    .toDate()
+            },
+            endtime: {
+                $gte: moment()
+                    .utcOffset(330)
+                    .toDate()
+            }
+        };
+
+        return Sales.findOne(salesQuery)
+            .exec()
+            .then(salesDoc => {
+                const durationLeft = moment()
+                    .utcOffset(330)
+                    .diff(moment(salesDoc.end));
+                if (durationLeft <= !3.1) {
+                    return done({ error: "duration is" + durationLeft });
+                }
+                /**
+                 * Use cursor to loop over all users
+                 */
+                return User.find({
+                    notif_subscribe: true
+                })
+                    .select({ username: 1, email: 1 })
+                    .cursor()
+                    .on("data", async user => {
+                        const ejsTemplate = await getEJSTemplate({
+                            fileName: "sale_about_end.ejs"
+                        });
+                        const finalHTML = ejsTemplate({
+                            time: moment().format("lll"),
+                            username: user.name,
+                            saleDetails: salesDoc //may be format properly before passing it
+                        });
+                        const message = {
+                            to: user.email,
+                            subject:
+                                "Sale is now live! pick it before you miss.",
+                            body: finalHTML
+                        };
+                        return await SendMail(message);
+                    })
+                    .on("error", err => {
+                        done(err);
+                    })
+                    .on("end", () => {
+                        salesDoc.notification_level = 2;
+                        salesDoc.save();
+                        return true;
+                    });
+            })
+            .catch(error => {
+                done(error);
+            });
     });
 };
 
