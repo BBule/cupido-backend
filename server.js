11629f2bad64d53d0aa5519b30c1590f45125906
saikat <saikatchakrabortty2@gmail.com>
2019-04-17 07:31:08 +0530

    [CLEAN-UP] removal unnecessery/unused codes

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const app = require("./app");
 const http = require("http");
-const mongoose = require("./db/connectDB");
+require("./db/connectDB");
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const config = require("./config/config");
 var server = http.createServer(app);
 server.listen(port, err => {
     if (err) throw err;
