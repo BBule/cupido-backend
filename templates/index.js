bf204c822cbc85ed949f06951bdbb745d42b0500
saikat <saikatchakrabortty2@gmail.com>
2019-04-17 13:32:10 +0530

    [REN] renamed file

--- templates/index.js
+++ templates/index.js
@@ -1,16 +1,16 @@
 const promotionals = {
-    sale_about_end: require("./promotional/sale_about_end"),
-    sale_is_live: require("./promotional/sale_is_live"),
-    sale_reminder_for_liked: require("./promotional/sale_reminder_for_liked")
+    "sale_about_end.ejs": require("./promotional/sale_about_end"),
+    "sale_is_live.ejs": require("./promotional/sale_is_live"),
+    "sale_reminder_for_liked.ejs": require("./promotional/sale_reminder_for_liked")
 };
 const transationals = {
-    gift_card_generated: require("./transational/gift_card_generated"),
-    missed_commit: require("./transational/missed_commit"),
-    order_delivered: require("./transational/order_delivered"),
-    order_shipped: require("./transational/order_shipped"),
-    pay_full_sellBuffer_start: require("./transational/pay_full_sellBuffer_start"),
-    pay_full_sellBuffer_end: require("./transational/pay_full_sellBuffer_end"),
-    payment_success: require("./transational/payment_success"),
-    thanks_commiting: require("./transational/thanks_commiting")
+    "gift_card.ejs": require("./transational/gift_card"),
+    "missed_commit.ejs": require("./transational/missed_commit"),
+    "order_delivered.ejs": require("./transational/order_delivered"),
+    "order_shipped.ejs": require("./transational/order_shipped"),
+    "pay_full_sellBuffer_start.ejs": require("./transational/pay_full_sellBuffer_start"),
+    "pay_full_sellBuffer_end.ejs": require("./transational/pay_full_sellBuffer_end"),
+    "payment_success.ejs": require("./transational/payment_success"),
+    "thanks_commitinge.ejs": require("./transational/thanks_commiting")
 };
 module.exports = { promotionals, transationals };
