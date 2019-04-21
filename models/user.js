97a81ab912a7792b2ea64d008f586efd641ae06f
saikat <saikatchakrabortty2@gmail.com>
2019-04-21 08:27:37 +0530

    [UPDATE] add timestamps to user signup

--- models/user.js
+++ models/user.js
@@ -4,31 +4,36 @@ const mongoose = require("mongoose");
 
 let Schema = mongoose.Schema;
 
-var UserSchema = new mongoose.Schema({
-    username: String,
-    email: {
-        email: String,
-        verified: Boolean
+var UserSchema = new mongoose.Schema(
+    {
+        username: String,
+        email: {
+            email: String,
+            verified: Boolean
+        },
+        contact: {
+            contact: Number,
+            verified: Boolean
+        },
+        notif_subscribe: {
+            type: Boolean,
+            default: true
+        },
+        googleId: String,
+        facebookId: String,
+        myorders: [Schema.Types.Mixed], //To be embedded
+        mycarts: [Schema.Types.Mixed], //To be embedded
+        mycommits: [Schema.Types.Mixed], //To be referenced ["ID001","ID002"]
+        mygifts: [Schema.Types.Mixed], //To be embedded
+        myaddresses: [Schema.Types.Mixed], //To be embedded
+        mynotifications: [Schema.Types.Mixed], //To be embedded
+        time_created: { type: Date, default: Date.now },
+        time_activated: { type: Date }
     },
-    contact: {
-        contact: Number,
-        verified: Boolean
-    },
-    notif_subscribe: {
-        type: Boolean,
-        default: true
-    },
-    googleId: String,
-    facebookId: String,
-    myorders: [Schema.Types.Mixed], //To be embedded
-    mycarts: [Schema.Types.Mixed], //To be embedded
-    mycommits: [Schema.Types.Mixed], //To be referenced ["ID001","ID002"]
-    mygifts: [Schema.Types.Mixed], //To be embedded
-    myaddresses: [Schema.Types.Mixed], //To be embedded
-    mynotifications: [Schema.Types.Mixed], //To be embedded
-    time_created: { type: Date, default: Date.now },
-    time_activated: { type: Date }
-});
+    {
+        timestamps: true
+    }
+);
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
