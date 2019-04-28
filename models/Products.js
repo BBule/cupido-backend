62b9c2b7f32fe803328a5293e81cf04266439d6b
saikat <saikatchakrabortty2@gmail.com>
2019-04-28 16:53:23 +0530

    change in product schema

--- models/Products.js
+++ models/Products.js
@@ -16,16 +16,21 @@ let ProductsSchema = new Schema(
         returnTime: Number,
         ShippingPrice: Number,
         images: [String],
+        backdrop_image: String,
         backdrop_title: String,
         backdrop_content: String,
+        whyweloveit_image: String,
         whyweloveit_title: String,
         whyweloveit_content: String,
+        brand_image: String,
         specifications_title: String,
         specifications_content: String,
         brand_title: String,
         brand_content: String,
         description: String,
         gender: Boolean, // 0 is Male
+        recomandation: [Schema.Types.ObjectId],
+        video: String,
         // Relevent Details
         // addedBy: {
         //     type: mongoose.Schema.Types.ObjectId,
