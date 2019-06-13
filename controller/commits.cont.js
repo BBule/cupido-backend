ddd7af76b8d00a11a27a53476dcf38195d648f5a
YASHYK47 <yashky47@gmail.com>
2019-06-13 10:52:13 +0530

    exitAPI

--- controller/commits.cont.js
+++ controller/commits.cont.js
@@ -2,6 +2,7 @@ const cartCont = require("./cart.cont");
 const mycommits = require("../models/mycommits");
 const myOrders = require("../models/myorders");
 const Saleslist = require("../models/saleslist");
+const cart = require("../models/mycartingeneral.js");
 
 const getUserCommits = async (
     userId,
@@ -27,8 +28,7 @@ const getUserOrders = async (
 ) => {
     return await myOrders
         .find({
-            "User.id": userId,
-            is_active: activeStat
+            "User.id": userId
         })
         .limit(limit)
         .skip(skip)
@@ -39,9 +39,16 @@ const getCommitCountBySale = async id => {
     return mycommits.countDocuments({ "sale.id": id }).exec();
 };
 
+// const getCupidQuantity=(async (cupidLove)=>{
+//     var count=0;
+//     cupidLove.forEach(item=>{
+//         count+=item.quantity;
+//     });
+//     return count;
+// })
+
 const createCommitOrOrder = async (wholeCart, addressId, payment, userId) => {
     let promiseArr = [];
-
     wholeCart.forEach(async element => {
         let commit_count = await getCommitCountBySale(element.sale.id);
         let sale = await Saleslist.findById(element.sale.id);
@@ -100,13 +107,139 @@ const createCommitOrOrder = async (wholeCart, addressId, payment, userId) => {
         promiseArr.push(cartCont.removeFromCart(wholeCart._id, userId));
     });
 
+    // wholeCart.forEach(async element => {
+    //     let commit_count = await getCommitCountBySale(element.sale.id);
+    //     let sale = await Saleslist.findById(element.sale.id);
+    //     if (element.is_commit && commit_count < sale.cupidLove.quantity) {
+    //         var commit1 = new mycommits({
+    //             ...element,
+    //             shipping_address: addressId,
+    //             payment_details: payment
+    //         });
+    //         promiseArr.push(commit1.save());
+    //         // Saleslist.findOneAndUpdate({_id:element.sale.id},{$inc: {quantity_committed:element.current_quantity_committed}});
+    //     } else if (
+    //         element.is_commit &&
+    //         commit_count >= sale.cupidLove.quantity
+    //     ) {
+    //         var order1 = new myOrders({
+    //             ...element,
+    //             shipping_address: addressId,
+    //             payment_details: payment
+    //         });
+    //         promiseArr.push(order1.save());
+    //         let commits = await mycommits.find({ "sale.id": element.sale.id });
+    //         commits.forEach(function(commit) {
+    //             promiseArr.push(mycommits.findByIdAndRemove(commit._id));
+    //             delete commit._id;
+    //             var order2 = new myOrders(commit);
+    //             promiseArr.push(order2.save());
+    //             // Saleslist.findOneAndUpdate({_id:element.sale.id},{$inc: {quantity_sold:element.current_quantity_committed}});
+    //         });
+    //     } else {
+    //         //order
+    //         var order1 = new myOrders({
+    //             ...element,
+    //             shipping_address: addressId,
+    //             payment_details: payment
+    //         });
+    //         promiseArr.push(order1.save());
+    //         // Saleslist.findOneAndUpdate({_id:element.sale.id},{$inc: {quantity_sold:element.current_quantity_committed}});
+    //     }
+    // });
+    // wholeCart.forEach(
+    //     async element => {
+    //         let commit_count = await getCommitCountBySale(element.sale.id);
+    //         console.log(commit_count);
+    //         Saleslist.findOne({ _id: element.sale.id }).then(async sale => {
+    //             if (
+    //                 element.is_commit &&
+    //                 commit_count < sale.cupidLove.quantity
+    //             ) {
+    //                 await new mycommits({
+    //                     ...element,
+    //                     shipping_address: addressId,
+    //                     payment_details: payment
+    //                 })
+    //                     .save()
+    //                     .then(async (err, result) => {
+    //                         if (err) {
+    //                             console.log(err);
+    //                         }
+    //                         await Saleslist.findOneAndUpdate(
+    //                             { _id: element.sale.id },
+    //                             {
+    //                                 $inc: {
+    //                                     quantity_committed:
+    //                                         element.current_quantity_committed
+    //                                 }
+    //                             }
+    //                         );
+    //                     });
+    //             } else if (element.is_commit && commit_count >= Cupidcount) {
+    //                 await new myOrders({
+    //                     ...element,
+    //                     shipping_address: addressId,
+    //                     payment_details: payment
+    //                 })
+    //                     .save()
+    //                     .then(async (err, result) => {
+    //                         if (err) {
+    //                             console.log(err);
+    //                         }
+    //                         let commits = await mycommits.find({
+    //                             "sale.id": element.sale.id
+    //                         });
+    //                         commits.forEach(async function(commit) {
+    //                             await mycommits.findByIdAndRemove(commit._id);
+    //                             delete commit._id;
+    //                             await new myOrders(commit).save();
+    //                             await Saleslist.findOneAndUpdate(
+    //                                 { _id: element.sale.id },
+    //                                 {
+    //                                     $inc: {
+    //                                         quantity_sold:
+    //                                             element.current_quantity_committed
+    //                                     }
+    //                                 }
+    //                             );
+    //                         });
+    //                     });
+    //             } else {
+    //                 await new myOrders({
+    //                     ...element,
+    //                     shipping_address: addressId,
+    //                     payment_details: payment
+    //                 })
+    //                     .save()
+    //                     .then(async (err, result) => {
+    //                         if (err) {
+    //                             console.log(err);
+    //                         }
+    //                         await Saleslist.findOneAndUpdate(
+    //                             { _id: element.sale.id },
+    //                             {
+    //                                 $inc: {
+    //                                     quantity_sold:
+    //                                         element.current_quantity_committed
+    //                                 }
+    //                             }
+    //                         );
+    //                     });
+    //             }
+    //         });
+    //     },
+    //     err => {
+    //         console.log(err);
+    //     }
+    // );
     return Promise.all(promiseArr)
-        .then(data => {
-            return { success: true };
-        })
-        .catch(error => {
-            return Promise.reject(error);
-        });
+    .then(data => {
+        return { success: true };
+    })
+    .catch(error => {
+        return Promise.reject(error);
+    });
 };
 
 module.exports = {
--- routes/commits.js
+++ routes/commits.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const myorders = require("../models/myorders.js");
+const mycommits = require("../models/mycommits.js");
+const Saleslist = require("../models/saleslist.js");
 
 const {
     createCommitOrOrder,
@@ -41,30 +44,150 @@ router.get("/myorders", (req, res, next) => {
 });
 
 router.post("/orderOrCommit", (req, res, next) => {
-    if(req.body.payment.id){
-        const { wholeCart, addressId ,payment} = req.body;
+    if (req.body.payment.id) {
+        const { wholeCart, addressId, payment } = req.body;
         if (!wholeCart || !wholeCart.length) {
             return next({
                 status: 400,
                 message: "please pass all the cart item"
             });
         }
-        createCommitOrOrder(wholeCart, addressId,payment, req.user._id)
+        // let promiseArr = [];
+        // wholeCart.forEach(async element => {
+        //     let commit_count = await getCommitCountBySale(element.sale.id);
+        //     let sale = await Saleslist.findById(element.sale.id);
+        //     if (element.is_commit && commit_count < sale.cupidLove.quantity) {
+        //         promiseArr.push(
+        //             await new mycommits({
+        //                 ...element,
+        //                 shipping_address: addressId,
+        //                 payment_details: payment
+        //             }).save()
+        //         );
+        //         await Saleslist.findOneAndUpdate(
+        //             { _id: element.sale.id },
+        //             {
+        //                 $inc: {
+        //                     quantity_committed:
+        //                         element.current_quantity_committed
+        //                 }
+        //             }
+        //         );
+        //     } else if (
+        //         element.is_commit &&
+        //         commit_count >= sale.cupidLove.quantity
+        //     ) {
+        //         promiseArr.push(
+        //             await new myorders({
+        //                 ...element,
+        //                 shipping_address: addressId,
+        //                 payment_details: payment
+        //             }).save()
+        //         );
+        //         let commits = await mycommits.find({
+        //             "sale.id": element.sale.id
+        //         });
+        //         commits.forEach(async function(commit) {
+        //             promiseArr.push(await mycommits.findByIdAndRemove(commit._id));
+        //             delete commit._id;
+        //             promiseArr.push( new myorders(commit).save());
+        //             await Saleslist.findOneAndUpdate(
+        //                 { _id: element.sale.id },
+        //                 {
+        //                     $inc: {
+        //                         quantity_sold:
+        //                             element.current_quantity_committed
+        //                     }
+        //                 }
+        //             );
+        //         });
+        //     } else {
+        //         //order
+        //         promiseArr.push(
+        //             await new myorders({
+        //                 ...element,
+        //                 shipping_address: addressId,
+        //                 payment_details: payment
+        //             }).save()
+        //         );
+        //         await Saleslist.findOneAndUpdate(
+        //             { _id: element.sale.id },
+        //             {
+        //                 $inc: {
+        //                     quantity_sold: element.current_quantity_committed
+        //                 }
+        //             }
+        //         );
+        //     }
+        //     // if (element.referral_code) {
+        //     //     promiseArr.push(
+        //     //         Referral.findOneAndUpdate(
+        //     //             { code: element.referral_code },
+        //     //             { used: true, usedBy: userId }
+        //     //         )
+        //     //     );
+        //     //     var referralBy = await Referral.findOne({
+        //     //         code: element.referral_code
+        //     //     }).select("createdBy");
+        //     //     promiseArr.push(
+        //     //         User.findByIdAndUpdate(referralBy, {
+        //     //             $inc: { cupidCoins: 50 }
+        //     //         })
+        //     //     );
+        //     // }
+        //     // promiseArr.push(cartCont.removeFromCart(wholeCart._id, userId));
+        // });
+        // Promise.all(promiseArr)
+        //     .then(result => {
+        //         return res.status(200).send(result);
+        //     })
+        //     .catch(err => {
+        //         return next({
+        //             status: 400,
+        //             message: "Unknown error occured!",
+        //             stack: err
+        //         });
+        //     });
+        createCommitOrOrder(wholeCart, addressId, payment, req.user._id)
             .then(data => {
-                return res.json(data);
+                return res.status(200).json(data);
             })
             .catch(error => {
-                return next({
-                    status: 400,
-                    message: "Unknown error occured!",
-                    stack: error
-                });
-            });
-    }else{
         return next({
-            status:400,
-            message:"please complete your payment"
-        })
+            status: 400,
+            message: "Unknown error occured!",
+            stack: error
+        });
+            });
+    } else {
+        return next({
+            status: 400,
+            message: "please complete your payment"
+        });
     }
 });
+
+// router.post("/exit", (req, res, next) => {
+//     let promiseArr = [];
+//     promiseArr.push(
+//         new myorders({
+//             ...req.body.wholeCart[0],
+//             "User.id": req.user._id,
+//             shipping_address: req.body.addressId,
+//             payment_details: req.body.payment
+//         }).save()
+//     );
+//     promiseArr.push(
+//         new mycommits({
+//             ...req.body.wholeCart[0],
+//             "User.id": req.user._id,
+//             shipping_address: req.body.addressId,
+//             payment_details: req.body.payment
+//         }).save()
+//     );
+//     Promise.all(promiseArr).then(() => {
+//         res.status(200).json({ msg: "done" });
+//     });
+// });
+
 module.exports = router;
