da6437aec6d2221fbe8e662c8e7e282de30bc8ba
Bhupendra Bule <bhupendra938@gmail.com>
2019-07-21 00:07:23 +0530

    Update products.js
    imported myorders

--- routes/unAuthRoutes/products.js
+++ routes/unAuthRoutes/products.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Orders = require("../../models/myorders");
 
 const productCont = require("../../controller/product.cont");
 
