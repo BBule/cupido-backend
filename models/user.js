c4ce7b451b72c8af26045f7b8777b9240ff46e3a
bbule <bhupendra938@gmail.com>
2019-08-01 21:44:18 +0530

    changed few things

--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@ var UserSchema = new mongoose.Schema(
         },
         gender: {
             type: String,
-            enum: ["male", "female"]
+            enum: ["male", "female", "Male", "Female"]
         },
         cupidCoins: {
             type: Number,
