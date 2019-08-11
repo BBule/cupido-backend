cd0df5293553c580e520e3ee3a0b8eb894c0c5a0
YASHYK47 <yashky47@gmail.com>
2019-08-11 18:53:58 +0530

    referral fixed finally

--- routes/referral.js
+++ routes/referral.js
@@ -9,50 +9,57 @@ const Saleslist = require("../models/saleslist");
 const Cupidlove = require("../models/CupidLove.js");
 
 router.post("/send", async (req, res, next) => {
-    await Saleslist.findOne({ _id: req.body.sale }).then(async sale => {
-        const salePrice = sale.salePrice;
-        const referralPercent = sale.referralPercent;
-        if (referralPercent) {
-            const amount = (referralPercent * salePrice) / 200;
-            var tokens = await Referral.find()
-                .select("code")
-                .then(async referral => {
-                    var token = randomize("Aa0", 5, { exclude: tokens });
-                    var referral = new Referral({
-                        code: token,
-                        createdBy: req.user._id,
-                        sale: req.body.sale,
-                        usedBy: [],
-                        // used: false,
-                        amount: amount
-                    });
-                    referral
-                        .save()
-                        .then(referral => {
-                            res.send({ code: referral.code });
-                        })
-                        .catch(err => {
-                            // console.log(err);
-                            return next({
-                                message: err || "unknown error",
-                                status: 400
+    Referral.findOne({sale:req.body.sale}).then(async referral=>{
+        if(referral){
+            return res.send({code:referral.code});
+        }
+        else{
+            await Saleslist.findOne({ _id: req.body.sale }).then(async sale => {
+                const salePrice = sale.salePrice;
+                const referralPercent = sale.referralPercent;
+                if (referralPercent) {
+                    const amount = (referralPercent * salePrice) / 200;
+                    var tokens = await Referral.find()
+                        .select("code")
+                        .then(async referral => {
+                            var token = randomize("Aa0", 5, { exclude: tokens });
+                            var referral = new Referral({
+                                code: token,
+                                createdBy: req.user._id,
+                                sale: req.body.sale,
+                                usedBy: [],
+                                // used: false,
+                                amount: amount
                             });
+                            referral
+                                .save()
+                                .then(referral => {
+                                    res.send({ code: referral.code });
+                                })
+                                .catch(err => {
+                                    // console.log(err);
+                                    return next({
+                                        message: err || "unknown error",
+                                        status: 400
+                                    });
+                                });
                         });
-                });
-        } else {
-            return next({
-                message: "Referral cannot be generated for this sale.",
-                status: 400
+                } else {
+                    return next({
+                        message: "Referral cannot be generated for this sale.",
+                        status: 400
+                    });
+                }
             });
         }
-    });
+    })
 });
 
 router.post("/apply", async (req, res, next) => {
     await Referral.findOne({
         code: req.body.code,
         // used: false,
-        cart:{$nin:[req.user._id]},
+        cart:{$nin:req.user._id},
         sale: req.body.sale,
         createdBy: { $ne: req.user._id }
     })
